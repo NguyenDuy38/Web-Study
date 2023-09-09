ec9c8f388a68e142f809c7cd4a4d7ddff412813b
Duy Nguyễn <104379213+Duy-07@users.noreply.github.com>
2023-09-09 14:30:07 +0700

    Initial commit

---
+++ JavaScript/main.js
@@ -0,0 +1,20 @@
+alert('Hi, JavaScript basic!');
+
+var fullName = 'DN-Javascript';
+var age = 21;
+
+console.log(fullName);
+console.warn(age);
+console.error('Error test');
+
+// confirm('Ban da du tuoi chua?');
+// prompt('Ban co loi nhan gi khong?');
+
+// setTimeout(function() {
+//     console.log(fullName);
+// }, 1000);
+
+// setInterval(function() {
+//     console.log(age + Math.random());
+// }, 1000);
+
